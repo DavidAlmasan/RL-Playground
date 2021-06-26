07d257cdf4fb97bcb79b8e8c086e31c6a6108474
David Almasan <davidalmasan@gmail.com>
2021-06-26 18:03:23 +0500

    some solver script

---
+++ rainbow/solver.py
@@ -0,0 +1,7 @@
+class Solver():
+    def __init__(self, config):
+        self.config = config
+        print(self.config)
+
+    def solve(self):
+        pass
--- rainbow/train.py
+++ rainbow/train.py
@@ -1,7 +1,15 @@
 """Top level script used to train an agent using Rainbow framework"""
 import warnings
+
+from config import get_config
+from solver import Solver
+
+
 def main(debug):
-    pass
+    config = get_config()
+    solver = Solver(config)
+    solver.solve()
+    print('Finished.')
 
 if __name__ == "__main__":
     debug = True
